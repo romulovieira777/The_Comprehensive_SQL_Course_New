91148876c9b39792905412724dd77d48b3fa374f
Romulo Vieira <romulo.vieira777@gmail.com>
2024-10-01 09:30:42 -0300

    Data Query Language

---
+++ Section_02_Data_Query_Language/01_The_Select_Statement.sql
@@ -0,0 +1,18 @@
+SELECT
+    regions.ID
+  , regions.Name
+FROM
+    countries.regions;
+
+
+SELECT
+    regions.ID
+FROM
+    countries.regions;
+
+
+SELECT
+    regions.NAME
+ ,  regions.ID
+FROM
+    countries.regions;
